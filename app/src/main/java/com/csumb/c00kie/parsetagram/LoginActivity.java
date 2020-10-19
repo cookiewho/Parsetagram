68e6c99d6e1e5555542bf3258507629e83d988cd
cookiewho <dinkwho@gmail.com>
2020-10-18 17:12:46 -0700

    post buttons work, taking an image is in the works

--- app/src/main/java/com/csumb/c00kie/parsetagram/LoginActivity.java
+++ app/src/main/java/com/csumb/c00kie/parsetagram/LoginActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
